73a5f6435ee8d2a899ca3d21f325029669da327c
rorygreig <rorygreig@gmail.com>
2015-05-09 17:39:15 +0100

    finished hash tables

--- HashTable.js
+++ HashTable.js
@@ -6,12 +6,44 @@ function HashTable() {
 	this.betterHash = betterHash;
 	this.showDistro = showDistro; 
 	this.put = put;
-	//this.get = get;
+	this.get = get;
+	this.buildChains = buildChains;
 }
 
-function put(data) {
-	var pos = this.betterHash(data); 
-	this.table[pos] = data;
+function buildChains(){
+	for(var i=0; i<this.table.length; i++){
+		this.table[i] = new Array();
+	}
+}
+
+function put(key) {
+	var pos = this.betterHash(key);
+	var index = 0;
+	if (this.table[pos][index] == undefined) {
+		this.table[pos][index+1] = key; 
+	} else {
+		index++;
+		while (this.table[pos][index] != undefined) { 
+			index++;
+		}
+		this.table[pos][index] = key; 
+	}
+}
+
+function get(key) {
+	var index = 0;
+	var hash = this.betterHash(key); 
+	if (this.table[pos][index] = key) {
+		return this.table[pos][index+1]; 
+	}
+	else {
+		index += 2; 
+		while (this.table[pos][index] != key) { 
+			index += 2;
+		}
+		return this.table[pos][index+1]; 
+	}
+	return undefined;
 }
 
 function simpleHash(data) {
@@ -22,23 +54,23 @@ function simpleHash(data) {
 	return total % this.table.length; 
 }
 
-function betterHash(string, arr) {
+function betterHash(str, arr) {
 	var H = 37;
 	var total = 0;
-	for (var i = 0; i < string.length; ++i) {
-		total += H * total + string.charCodeAt(i);
+	for (var i = 0; i < str.length; ++i) {
+		total += H * total + str.charCodeAt(i);
 	}
 	total = total % this.table.length;
 	return parseInt(total); 
 }
 
 function showDistro() { 
+//	console.log(this.table);
 	for (var i = 0; i < this.table.length; ++i) { 
-		if (this.table[i] != undefined) {
+		if (this.table[i][0] != undefined) {
 			print(i + ": " + this.table[i]); 
 		}
-	} 
+	}
 }
-
 
 module.exports = HashTable;
--- testHashTable.js
+++ testHashTable.js
@@ -1,8 +1,52 @@
 var print = console.log;
 var HashTable = require('./HashTable.js');
 
+var hTable = new HashTable();
+hTable.buildChains();
 var someNames = ["David", "Jennifer", "Donnie", "Raymond",
-"Cynthia", "Mike", "Clayton", "Danny", "Jonathan"]; var hTable = new HashTable();
-for (var i = 0; i < someNames.length; ++i) { hTable.put(someNames[i]);
+		"Cynthia", "Mike", "Clayton", "Danny", "Jonathan"]; 
+for (var i = 0; i < someNames.length; ++i) {
+   hTable.put(someNames[i]);
 }
 hTable.showDistro();
+
+
+//var someNames = ["David", "Jennifer", "Donnie", "Raymond",
+//"Cynthia", "Mike", "Clayton", "Danny", "Jonathan"]; 
+//var hTable = new HashTable();
+//for (var i = 0; i < someNames.length; ++i) { hTable.put(someNames[i]);
+//}
+//hTable.showDistro();
+
+//
+//var numStudents = 10; 
+//var arrSize = 97; 
+//var idLen = 9;
+//var students = new Array(numStudents); 
+//genStuData(students);
+//print ("Student data: \n");
+//for (var i = 0; i < students.length; ++i) {
+//   print(students[i].substring(0,8) + " " +
+//         students[i].substring(9));
+//}
+//print("\n\nData distribution: \n");
+//var hTable = new HashTable();
+//for (var i = 0; i < students.length; ++i) {
+//   hTable.put(students[i]);
+//}
+//hTable.showDistro();
+//
+//function getRandomInt (min, max) {
+//	return Math.floor(Math.random() * (max - min + 1)) + min;
+//}
+//function genStuData(arr) {
+//	for (var i = 0; i < arr.length; ++i) {
+//		var num = ""; 
+//		for(var j=1;j<=9;++j){
+//			num += Math.floor(Math.random() * 10);
+//		}
+//		num += getRandomInt(50,100);
+//		arr[i] = num;
+//	}
+//}
+
