c135154eb21322b2a3380d0c78a5c578c9bee44d
rorygreig <rorygreig@gmail.com>
2015-05-10 11:25:26 +0100

    graph with bfs and dfs

--- Graph.js
+++ Graph.js
@@ -4,7 +4,6 @@ function Graph(v){
 	this.adjacencyList = [];
 	for(var i =0; i < this.vertices; i++){
 		this.adjacencyList[i] = [];
-		this.adjacencyList[i].push("");
 	}
 }
 
@@ -12,9 +11,9 @@ Graph.prototype.addEdge = function(v,w){
 	this.adjacencyList[v].push(w);
 	this.adjacencyList[w].push(v);
 	this.edges++;
-}
+};
 
-Graph.prototype.showGraph = function() {
+Graph.prototype.showGraph = function(){
 	for (var i = 0; i < this.vertices; ++i) {
 		var str = i + " -> ";
 		for (var j = 0; j < this.vertices; ++j) {
@@ -22,6 +21,50 @@ Graph.prototype.showGraph = function() {
 				str += this.adjacencyList[i][j] + ' ';
 		}
 		console.log(str); 
+	}
+};
+
+Graph.prototype.dfs = function(s, visitFunc){
+	var marked = [];
+	for(var i = 0; i<this.vertices; i++){
+		marked[i] = false;
+	}
+	var stack = [];
+	marked[s] = true;
+	stack.push(s);
+	while(stack.length > 0){
+		var v = stack.pop();
+		if(v != undefined){
+			visitFunc(v);
+		}
+		for( var w = 0; w < this.adjacencyList.length; w++){
+			if(!marked[w]){
+				marked[w] = true;
+				stack.push(w);
+			}
+		}
+	}
+};
+
+Graph.prototype.bfs = function(s, visitFunc){
+	var marked = [];
+	for(var i = 0; i<this.vertices; i++){
+		marked[i] = false;
+	}
+	var queue = [];
+	marked[s] = true;
+	queue.push(s);
+	while(queue.length > 0){
+		var v = queue.shift();
+		if(v != undefined){
+			visitFunc(v);
+		}
+		for( var w = 0; w < this.adjacencyList.length; w++){
+			if(!marked[w]){
+				marked[w] = true;
+				queue.push(w);
+			}
+		}
 	}
 }
 
--- testGraph.js
+++ testGraph.js
@@ -1,8 +1,12 @@
 var Graph = require('./Graph.js');
 
-g = new Graph(5);
+var g = new Graph(5); 
 g.addEdge(0,1); 
 g.addEdge(0,2); 
 g.addEdge(1,3); 
 g.addEdge(2,4); 
-g.showGraph();
+g.showGraph(); 
+g.bfs(0, function(v){
+	console.log('visited vertex: ' + v);
+});
+
