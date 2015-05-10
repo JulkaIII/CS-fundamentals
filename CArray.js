4366ed89eebf513d2f34a75e5cee8e64920ea580
rorygreig <rorygreig@gmail.com>
2015-05-10 16:23:19 +0100

    binary search

--- CArray.js
+++ CArray.js
@@ -40,4 +40,15 @@ CArray.prototype.swap = function(arr, index1, index2){
 	arr[index2] = temp;
 };
 
+CArray.prototype.bubbleSort = function(){
+	var numElements = this.dataStore.length;
+	for (var outer = numElements; outer >= 2; --outer) {
+		for (var inner = 0; inner <= outer-1; ++inner) {
+			if (this.dataStore[inner] > this.dataStore[inner+1]) {
+				this.swap(this.dataStore, inner, inner+1); 
+			}
+		} 
+	}
+};
+
 module.exports = CArray;
---
+++ binSearch.js
@@ -0,0 +1,33 @@
+var nums = []; for(var i=0;i<100;++i){
+   nums[i] = Math.floor(Math.random() * 101);
+}
+
+nums.sort(function(a, b){
+	return a - b;
+});
+console.log(nums);
+
+var val = 40;
+
+var retVal = binSearch(nums, val); 
+if (retVal >= 0) {
+  	console.log("Found " + val + " at position " + retVal);
+}
+else {
+	console.log(val + " is not in array.");
+}
+
+function binSearch(arr, data) {
+	var upperBound = arr.length-1; 
+	var lowerBound = 0;
+	while (lowerBound <= upperBound) {
+		var mid = Math.floor((upperBound + lowerBound) / 2); 
+		if (arr[mid] < data) {
+		         lowerBound = mid + 1;
+		} else if (arr[mid] > data) { 
+			upperBound = mid - 1;
+		} else {
+		return mid; }
+	}
+	return -1; 
+}
